7b8174b49a8d0f435c9b0dba38ea8cad463ba45b
thibault-manse <thibault.manse@laplateforme.io>
2025-06-13 14:27:00 +0200

    Update - add td with debug for creating Tower, New Tower and ennemies type

--- Game.cpp
+++ Game.cpp
@@ -1,5 +1,6 @@
 #include "Game.h"
 #include <algorithm>
+#include <cmath>
 
 Game::Game(int width, int height)
     : money(3000), lives(10), enemySpawnTimer(0), enemySpawnInterval(2.0f),
@@ -25,14 +26,19 @@ void Game::Update(float deltaTime)
     enemySpawnTimer += deltaTime;
     if (enemySpawnTimer >= enemySpawnInterval)
     {
-        enemies.emplace_back(path[0]);
+        int typeRand = GetRandomValue(0, 9);
+        EnemyType type = NORMAL;
+        if (typeRand < 6) type = NORMAL;      // 60% de chance
+        else if (typeRand < 9) type = ATTACK; // 30% de chance
+        else type = BOSS;                     // 10% de chance
+        enemies.emplace_back(path[0], 50.0f, type);
         enemySpawnTimer = 0;
     }
 
     // Update enemies
     for (auto &enemy : enemies)
     {
-        enemy.Update(deltaTime, path);
+        enemy.Update(deltaTime, path, towers);
 
         if (enemy.currentWaypoint >= path.size() && enemy.alive)
         {
@@ -54,7 +60,8 @@ void Game::Update(float deltaTime)
     // Update towers
     for (auto &tower : towers)
     {
-        tower.Update(deltaTime, enemies);
+        if (!tower.destroyed)
+            tower.Update(deltaTime, enemies);
     }
 
     // Tower placement
@@ -62,9 +69,19 @@ void Game::Update(float deltaTime)
     {
         tempTower.position = GetMousePosition();
 
+        if (waitingForMouseRelease)
+        {
+            // On attend que le bouton soit relâché avant d'autoriser le placement
+            if (!IsMouseButtonDown(MOUSE_LEFT_BUTTON))
+            {
+                waitingForMouseRelease = false;
+            }
+            return;
+        }
+
         if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
         {
-            if (money >= tempTower.cost)
+            if (money >= tempTower.cost && !IsOnPath(tempTower.position))
             {
                 towers.push_back(tempTower);
                 money -= tempTower.cost;
@@ -77,6 +94,38 @@ void Game::Update(float deltaTime)
             placingTower = false;
         }
     }
+
+    if (showTowerMenu)
+    {
+        Vector2 mousePos = GetMousePosition();
+
+        // Exemple de 3 boutons verticaux pour chaque type de tour
+        Rectangle basicBtn = {700, 50, 80, 30};
+        Rectangle laserBtn = {700, 90, 80, 30};
+        Rectangle poisonBtn = {700, 130, 80, 30};
+
+        if (CheckCollisionPointRec(mousePos, basicBtn) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            towerTypeToBuild = BASIC;
+            placingTower = true;
+            waitingForMouseRelease = true;
+            tempTower = Tower({0, 0}, 80, 10, 1.0f, BASIC);
+            showTowerMenu = false;
+        }
+        if (CheckCollisionPointRec(mousePos, laserBtn) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            towerTypeToBuild = LASER;
+            placingTower = true;
+            waitingForMouseRelease = true;
+            tempTower = Tower({0, 0}, 100, 5, 0.2f, LASER);
+            showTowerMenu = false;
+        }
+        if (CheckCollisionPointRec(mousePos, poisonBtn) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            towerTypeToBuild = POISON;
+            placingTower = true;
+            waitingForMouseRelease = true;
+            tempTower = Tower({0, 0}, 70, 2, 1.5f, POISON);
+            showTowerMenu = false;
+        }
+    }
 }
 
 void Game::Draw()
@@ -84,7 +133,7 @@ void Game::Draw()
     // Draw path
     for (size_t i = 1; i < path.size(); ++i)
     {
-        DrawLineV(path[i - 1], path[i], YELLOW);
+        DrawLineV(path[i - 1], path[i], YELLOW); // Ligne centrale
     }
 
     // Draw enemies
@@ -96,7 +145,8 @@ void Game::Draw()
     // Draw towers
     for (auto &tower : towers)
     {
-        tower.Draw();
+        if (!tower.destroyed)
+            tower.Draw();
     }
 
     // Draw tower being placed
@@ -117,6 +167,25 @@ void Game::Draw()
     {
         DrawText("GAME OVER", screenWidth / 2 - 100, screenHeight / 2 - 25, 50, RED);
     }
+
+    if (showTowerMenu)
+    {
+        DrawRectangle(700, 50, 80, 30, LIGHTGRAY);
+        DrawText("Basique", 705, 55, 12, BLACK);
+        DrawRectangle(700, 90, 80, 30, LIGHTGRAY);
+        DrawText("Laser", 705, 95, 12, BLACK);
+        DrawRectangle(700, 130, 80, 30, LIGHTGRAY);
+        DrawText("Poison", 705, 135, 12, BLACK);
+    }
+
+    for (const auto& enemy : enemies) {
+        if (enemy.type == ATTACK && enemy.lastAttackTowerIndex != -1 && enemy.attackLaserTimer > 0.0f) {
+            const auto& tower = towers[enemy.lastAttackTowerIndex];
+            if (!tower.destroyed) {
+                DrawLineEx(enemy.position, tower.position, 4, RED);
+            }
+        }
+    }
 }
 
 void Game::ProcessInput()
@@ -124,12 +193,34 @@ void Game::ProcessInput()
     if (IsKeyPressed(KEY_ESCAPE))
     {
         placingTower = false;
+        waitingForMouseRelease = false;
     }
 
     Vector2 mousePos = GetMousePosition();
     if (CheckCollisionPointRec(mousePos, {700, 10, 80, 30}) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
     {
-        placingTower = true;
-        tempTower = Tower({0, 0});
+        showTowerMenu = true;
+        placingTower = false;
+        waitingForMouseRelease = false;
     }
+}
+
+bool Game::IsOnPath(Vector2 point) const
+{
+    for (size_t i = 1; i < path.size(); ++i)
+    {
+        Vector2 a = path[i - 1];
+        Vector2 b = path[i];
+        
+        float dx = b.x - a.x;
+        float dy = b.y - a.y;
+        float length2 = dx * dx + dy * dy;
+        float t = ((point.x - a.x) * dx + (point.y - a.y) * dy) / length2;
+        t = fmaxf(0, fminf(1, t));
+        Vector2 proj = { a.x + t * dx, a.y + t * dy };
+        float dist2 = (point.x - proj.x) * (point.x - proj.x) + (point.y - proj.y) * (point.y - proj.y);
+        if (dist2 < (50 / 2) * (50 / 2))
+            return true;
+    }
+    return false;
 }
--- Tower.h
+++ Tower.h
@@ -6,20 +6,26 @@
 
 struct Enemy; // Forward declaration
 
+enum TowerType { BASIC, LASER, POISON };
+
 struct Tower
 {
     Vector2 position;
     float range;
     float damage;
+    int health = 100; 
+    bool destroyed;
     float cooldown;
     float cooldownTimer;
     int cost;
     Color color;
+    TowerType type;
 
     Tower();
-    Tower(Vector2 pos, float rng = 80.0f, float dmg = 10.0f, float cd = 1.0f);
+    Tower(Vector2 pos, float rng = 80.0f, float dmg = 10.0f, float cd = 1.0f, TowerType t = BASIC);
     void Update(float deltaTime, std::vector<Enemy> &enemies);
     void Attack(std::vector<Enemy> &enemies);
+    void TakeDamage(int dmg);
     void Draw(bool showRange = false);
 };
 
